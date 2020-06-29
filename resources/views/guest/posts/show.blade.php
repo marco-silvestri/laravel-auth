18fefae38c0221215b49a08272dab94ad168e1c7
marco-silvestri <m.silvestri03@gmail.com>
2020-06-30 00:35:54 +0200

    Comments added in the guest view (show)

--- resources/views/guest/posts/show.blade.php
+++ resources/views/guest/posts/show.blade.php
@@ -6,9 +6,14 @@
         <div class="card" style="width: 18rem;">
             <div class="card-body">
                 <h5 class="card-title">{{ $post->title }} (last updated: {{ $post->updated_at }})</h5>
-                <h6 class="card-subtitle mb-2 text-muted">Original author: {{ $post->user_id }}</h6>
+                <h6 class="card-subtitle mb-2 text-muted">Original author: {{ $post->user->name }}</h6>
                 <p class="card-text">{{ $post->body }}</p>
             </div>
         </div>
+        @foreach ($post->comments as $comment)
+            <p>Comment {{ $loop->iteration }}</p>
+            <p>{{ $comment->title }}</p>
+            <p>{{ $comment->body }}</p>
+        @endforeach
     </div>
 @endsection
